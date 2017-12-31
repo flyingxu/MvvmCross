f1cfda701255842b28656e218fc17bb76779dac1
flyingxu <flyingxu@gmail.com>
2017-12-31 11:00:37 +0800

    Update Playground.Forms.Mac project

---
+++ TestProjects/Playground/Playground.Forms.Mac/Setup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AppKit;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Forms.iOS;
+using MvvmCross.Mac.Platform;
+using MvvmCross.Platform.Platform;
+
+namespace Playground.Forms.Mac
+{
+    public class Setup : MvxFormsMacSetup
+    {
+        public Setup(IMvxApplicationDelegate applicationDelegate, NSWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+
+        protected override MvvmCross.Forms.Platform.MvxFormsApplication CreateFormsApplication()
+        {
+            return new FormsApp();
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+
+        protected override IEnumerable<Assembly> GetViewAssemblies()
+        {
+            return new List<Assembly>(base.GetViewAssemblies().Union(new[] { typeof(FormsApp).GetTypeInfo().Assembly }));
+        }
+    }
+}
